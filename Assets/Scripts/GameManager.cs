4ae28f26b8e3ef23239fb810d8d3e2ee6d8011c5
Arrcival <arrcival@gmail.com>
2020-05-31 19:53:09 +0200

    J'ai fais des bails, vraiment

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public List<LevelManager> levels;
+    public Canvas canvas;
+
+
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape) && canvas.enabled)
+            QuitApplication();
+    }
+
+    public void QuitApplication()
+    {
+        Application.Quit();
+    }
+
+    public void LoadLevel(int level)
+    {
+        if(level <= levels.Count && level > 0)
+        {
+            Instantiate(levels[level - 1]);
+            canvas.enabled = false;
+        }
+    }
+
+    public void GameOver(bool won)
+    {
+        canvas.enabled = true;
+    }
+}
